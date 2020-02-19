a7fe438f046e768161156272eb1886a07761570d
Li Jiang <lijgame@gmail.com>
2020-02-19 09:57:01 +0800

    build.bat

---
+++ build.bat
@@ -0,0 +1,1 @@
+cmake -G"Visual Studio 16 2019" -A Win64 -DCMAKE_TOOLCHAIN_FILE=C:/codes/vcpkg/scripts/buildsystems/vcpkg.cmake -DMYSQL_EXECUTABLE=C:/Apps/mysql-5.6.26-winx64/bin/mysql.exe -DMYSQL_INCLUDE_DIR=C:/Apps/mysql-5.6.26-winx64/include -DMYSQL_LIBRARY=C:/Apps/mysql-5.6.26-winx64/lib/libmysql.lib ..
